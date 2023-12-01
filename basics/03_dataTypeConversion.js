91cc7494af4891b24e51bb88006c710c2074ce78
Vishal Dogra <dogravishal1598@gmail.com>
2023-12-01 21:20:58 +0530

    operations in js

--- basics/03_dataTypeConversion.js
+++ basics/03_dataTypeConversion.js
@@ -72,5 +72,33 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let num = 33;
 
 let stringNum = String(num);
-console.log(typeof stringNum); // string
-console.log(stringNum); // "33"
+// console.log(typeof stringNum); // string
+// console.log(stringNum); // "33"
+
+// ************************** Operations ****************************
+
+let value = 5;
+let negValue = -value;
+// console.log(negValue); // -5
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 5); // 2^5
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2;
+// console.log(str3); // "hello world"
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+
+// don't write this type of code
+console.log(+true); // 1
+console.log(+""); // 0

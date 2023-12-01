afd03a6368b015239df2d69e0cb3e82d133146eb
Vishal Dogra <dogravishal1598@gmail.com>
2023-12-01 21:44:57 +0530

    data types cod eupdate

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,14 @@
 "use strict"; // treat all JS code as newer version
 
 /*
+    2 category of data types:
+        primitive (call by value)
+            -- String, Number, Boolean, null, undefined, Symbol, BigInt
+        Reference(Non-premitive) (call by reference)
+            -- Object, Array, Functions
+*/
+
+/*
     Js has dynamic typing, i.e, we don't have to manually define the data type of the value stored in a variable. Instead data types are determined automatically.
     ** Note: Value has type, NOT variable
 
@@ -30,11 +38,21 @@
              8. "function"
 */
 
-console.log(typeof 123); // number
-console.log(typeof 12.45); // number
-console.log(typeof "hello"); // string
-console.log(typeof true); // boolean
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof 123); // number
+// console.log(typeof 12.45); // number
+// console.log(typeof "hello"); // string
+// console.log(typeof true); // boolean
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
 
+const score = 100;
+const scodeValue = 100.5;
+let isloggedIn = false;
+let userEmail;
 let temperature = null; // value is not undefined, intentionally setting value as null
+
+const id1 = Symbol("123");
+const id2 = Symbol("123");
+console.log(id1 === id2); // false
+
+const bigNumber = 782364874632984023472390874n;
